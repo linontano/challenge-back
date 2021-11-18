44caa1bef5bc0dce5cb893b7e46cdc557d3cab3a
Lino Ontano Viteri <lianontano@gmail.com>
2021-11-18 06:24:16 -0500

    Finish API Controller, update README.md

--- src/README.md
+++ src/README.md
@@ -1,2 +1,12 @@
 # Challenge - Laravel API with Vue.js
 A continuación está detallado los métodos que se utilizan en el API.
+## Retornar todos los vuelos
+```
+GET /api/vuelos
+200 [{ id, name, destination, origin, departure_date, arrival_date, created_at, updated_at }]
+```
+## Retornar el detalle de un vuelo específico
+```
+GET /api/vuelos
+200 { id, flight_id, price, airline, number_scales, duration_hours, created_at, updated_at }
+```
---
+++ src/app/Http/Controllers/FlightController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Flight;
+use App\Models\DetailFlight;
+use Illuminate\Http\Request;
+
+class FlightController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Flight::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function showDetail($id)
+    {
+        $detail = DetailFlight::where('flight_id',$id)->get();
+        if (sizeof($detail)==0){
+            $detail = ["No existe vuelo"];
+        }
+        return $detail[0];
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
